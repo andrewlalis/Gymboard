f3aec7b2a2c36330f1a5a7d973dcff9f8d19cbce
Andrew Lalis <andrewlalisofficial@gmail.com>
2023-01-19 21:49:22 +0100

    Added styling and gympage.

---
+++ gymboard-app/src/pages/GymPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page>
+    Hello Gym Page
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'GymPage',
+    setup() {
+        return {};
+    }
+});
+</script>
--- gymboard-app/src/router/routes.ts
+++ gymboard-app/src/router/routes.ts
@@ -1,10 +1,22 @@
 import { RouteRecordRaw } from 'vue-router';
+import MainLayout from 'layouts/MainLayout.vue';
+import IndexPage from 'pages/IndexPage.vue';
+import GymPage from 'pages/GymPage.vue';
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: MainLayout,
+    children: [
+      {
+        path: '',
+        component: IndexPage
+      },
+      {
+        path: 'gyms/:id',
+        component: GymPage
+      }
+    ],
   },
 
   // Always leave this as last one,
