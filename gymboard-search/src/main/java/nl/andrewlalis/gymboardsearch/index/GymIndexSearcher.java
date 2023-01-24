548d0a7397dcf76afe0ad892a961c54e632a0150
Andrew Lalis <andrewlalisofficial@gmail.com>
2023-01-24 17:41:20 +0100

    Improved searcher with wildcards.

--- gymboard-search/src/main/java/nl/andrewlalis/gymboardsearch/index/GymIndexSearcher.java
+++ gymboard-search/src/main/java/nl/andrewlalis/gymboardsearch/index/GymIndexSearcher.java
@@ -10,34 +10,42 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+/**
+ * Searcher that uses a Lucene {@link IndexSearcher} to search for gyms using
+ * a query that's built from a weighted list of wildcard search terms.
+ * <ol>
+ *     <li>If the query is blank, return an empty list.</li>
+ *     <li>Split the query into words, append the wildcard '*' to each word.</li>
+ *     <li>For each word, add a boosted wildcard query for each weighted field.</li>
+ * </ol>
+ */
 @Service
 public class GymIndexSearcher {
 	public List<GymResponse> searchGyms(String rawQuery) {
 		if (rawQuery == null || rawQuery.isBlank()) return Collections.emptyList();
 		String[] terms = rawQuery.split("\\s+");
 		BooleanQuery.Builder queryBuilder = new BooleanQuery.Builder();
-		String[] searchableFields = {
-			"short_name",
-			"display_name",
-			"city_short_name",
-			"city_name",
-			"country_code",
-			"country_name",
-			"street_address"
-		};
+		Map<String, Float> fieldWeights = new HashMap<>();
+		fieldWeights.put("short_name", 3f);
+		fieldWeights.put("display_name", 3f);
+		fieldWeights.put("city_short_name", 1f);
+		fieldWeights.put("city_name", 1f);
+		fieldWeights.put("country_code", 0.25f);
+		fieldWeights.put("country_name", 0.5f);
+		fieldWeights.put("street_address", 0.1f);
 		for (String term : terms) {
-			for (String field : searchableFields) {
-				queryBuilder.add(new TermQuery(new Term(field, term)), BooleanClause.Occur.SHOULD);
+			String searchTerm = term.strip() + "*";
+			for (var entry : fieldWeights.entrySet()) {
+				Query baseQuery = new WildcardQuery(new Term(entry.getKey(), searchTerm));
+				queryBuilder.add(new BoostQuery(baseQuery, entry.getValue()), BooleanClause.Occur.SHOULD);
 			}
 		}
 		BooleanQuery query = queryBuilder.build();
 		Path gymIndexDir = Path.of("gym-index");
 		try (
-				var reader = DirectoryReader.open(FSDirectory.open(gymIndexDir));
+				var reader = DirectoryReader.open(FSDirectory.open(gymIndexDir))
 		) {
 			IndexSearcher searcher = new IndexSearcher(reader);
 			List<GymResponse> results = new ArrayList<>(10);
