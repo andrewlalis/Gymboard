40891656d03c2e771cb61a39423077d53b3ae8fb
Andrew Lalis <andrewlalisofficial@gmail.com>
2023-02-16 08:22:19 +0100

    Added more settings stuff.

--- gymboard-app/src/pages/auth/UserSettingsPage.vue
+++ gymboard-app/src/pages/auth/UserSettingsPage.vue
@@ -2,12 +2,33 @@
   <q-page>
     <StandardCenteredPage>
       <h3>{{ $t('userSettingsPage.title') }}</h3>
+      <hr>
+      <q-form>
+        
+      </q-form>
     </StandardCenteredPage>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import StandardCenteredPage from 'components/StandardCenteredPage.vue';
+import { User } from 'src/api/main/auth';
+import { getUserRoute } from 'src/router/user-routing';
+import { useAuthStore } from 'src/stores/auth-store';
+import { onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+
+const route = useRoute();
+const router = useRouter();
+const authStore = useAuthStore();
+
+onMounted(async () => {
+  // If the user isn't on their own settings page, redirect them back to the user's profile page.
+  const userId = route.params.userId as string;
+  if (authStore.user?.id !== userId) {
+    await router.push(`/users/${userId}`);
+  }
+});
 </script>
 
 <style scoped>
